781507132f3b3e1fb03b02be0fe6ef4481caf800
Xing-Huang <hxg@palliums.org>
2020-11-03 10:14:56 +0800

    change api-key

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
-api_key = ""
-secret_key = ""
-passphrase = ""
+api_key = "dfc629f5-2235-44e9-b9ec-57334f19484d"
+secret_key = "13583744689edc"
+passphrase = "3DA4510B00E54A29C90CCF895D48F42B"
 multiple_time = 10
 buy_trigger_ratio = 1+0.02/multiple_time
 sell_trigger_ratio = 1-0.02/multiple_time
