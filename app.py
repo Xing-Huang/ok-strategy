1095e182cfa7035f62e29dfff9b8e4c4f505b725
Xing-Huang <hxg@palliums.org>
2020-11-03 16:17:09 +0800

    add buy

--- app.py
+++ app.py
@@ -20,6 +20,20 @@ def index():
     ret["trade_detail"] = btc_market.get_trade_detail()
     return ret
 
+@app.route("/buy")
+def buy():
+    btc_market = strategy_thread.btc_market
+    cur_price = btc_market.get_mark_price()
+    btc_market.buy(cur_price, size=size, target_profit=target_profit, stop_loss=stop_loss)
+    return "success"
+
+@app.route("/sell")
+def buy():
+    btc_market = strategy_thread.btc_market
+    cur_price = btc_market.get_mark_price()
+    btc_market.sell(cur_price, size=size, target_profit=target_profit, stop_loss=stop_loss)
+    return "success"
+
 if __name__ == "__main__":
     strategy_thread.setDaemon(True)
     strategy_thread.start()
